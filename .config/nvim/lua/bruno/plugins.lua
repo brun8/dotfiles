8359f0399179e06bd3bb53a187ca7b19f62b43f4
bruno <brunofvteixeira@gmail.com>
2021-10-23 00:36:53 -0300

    telescope ignore node_modules

--- .config/nvim/lua/bruno/plugins.lua
+++ .config/nvim/lua/bruno/plugins.lua
@@ -69,7 +69,8 @@ end
 local function plugin_configs()
   require'telescope'.setup{
     defaults = {
-      prompt_prefix = '🍛 '
+      prompt_prefix = '🍛 ',
+      file_ignore_patterns = { "node%_modules/.*" }
     },
     extensions = {
       fzf = {
@@ -77,9 +78,10 @@ local function plugin_configs()
         override_generic_sorter = false, -- override the generic sorter
         override_file_sorter = true,     -- override the file sorter
         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-      }
-    }
+      },
+    },
   }
+
   require'telescope'.load_extension('fzf')
 
   require("todo-comments").setup {}
