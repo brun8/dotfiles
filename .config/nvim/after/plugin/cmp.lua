2cb68194f60df222c7339f898dda94cd7ad3b004
bruno <brunofvteixeira@gmail.com>
2021-10-24 18:16:46 -0300

    after dir

---
+++ .config/nvim/after/plugin/cmp.lua
@@ -0,0 +1,47 @@
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` user.
+      -- vim.fn["vsnip#anonymous"](args.body)
+
+      -- For `luasnip` user.
+      -- require('luasnip').lsp_expand(args.body)
+
+      -- For `ultisnips` user.
+      -- vim.fn["UltiSnips#Anon"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-CR>'] = cmp.mapping.confirm({select = true}),
+  },
+  sources = {
+    { name = 'gh_issues' },
+    { name = 'buffer'},
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    --{ name = 'emoji', opts = {insert = true} },
+
+    -- For vsnip user.
+    --{ name = 'vsnip' },
+  },
+  experimental = {
+    ghost_text = true,
+  }
+})
+
+
+-- Setup lspconfig.
+local servers = require'bruno.servers'
+
+for _, s in ipairs(servers) do
+  require('lspconfig')[s].setup {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  }
+end
+
