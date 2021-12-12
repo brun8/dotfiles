6fec1066ad2924942d9e44c8174cc5de44d0219c
bruno <brunofvteixeira@gmail.com>
2021-12-12 20:34:09 -0300

    cmp

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -18,6 +18,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
+    ['<C-CR>'] = cmp.mapping.close(),
     --['<C-CR>'] = cmp.mapping.confirm({select = true}),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
