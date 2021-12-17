af16682f3cc68580971421e98f07cfbdf902e3a3
bruno <brunofvteixeira@gmail.com>
2021-12-17 18:53:52 -0300

    wallpaper

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -119,25 +119,10 @@ end
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
-local function set_wallpaper(s)
-  -- Wallpaper
-  if beautiful.wallpaper then
-    local wallpaper = beautiful.wallpaper
-    -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
-      wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
-  end
-end
-
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
+--screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
-  -- Wallpaper
-  set_wallpaper(s)
-
   -- Each screen has its own tag table.
   awful.tag({ "1", "2", "3", "4" }, s, awful.layout.layouts[2])
 end)
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,7 +15,9 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar top-left 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example &
+#echo "Bars launched..."
