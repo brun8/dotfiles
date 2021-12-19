d19f91f9cbcc5ba3bf3f2a11dbe209d746b3fb35
bruno <brunofvteixeira@gmail.com>
2021-12-19 00:16:20 -0300

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -19,5 +19,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 #echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example &
+polybar bar &
 #echo "Bars launched..."
