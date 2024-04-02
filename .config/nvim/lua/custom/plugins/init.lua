a3e9bb8b50813bd86e68cdc0e567288a4d95c271
bruno <brunoteixeira@fastmail.com>
2024-04-02 19:10:28 -0300

    emmet

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'mattn/emmet-vim',
+    init = function()
+      vim.g['user_emmet_leader_key'] = '<C-e>'
+    end,
+  },
+}
