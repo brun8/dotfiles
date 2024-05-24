bf4d1b5ba170c1ef1a2037371672a02890c8f6b5
bruno <brunofvteixeira@gmail.com>
2024-05-24 14:31:26 -0300

    remove <C-h>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -106,10 +106,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -682,6 +682,7 @@ require('lazy').setup({
     init = function()
       vim.o.background = 'light'
       vim.cmd.colorscheme 'zenbones'
+      -- vim.cmd.colorscheme 'kanagawabones'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
